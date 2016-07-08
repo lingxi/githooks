a6dd07d06bf52f95d7316720b671027ba05ba23d
wj <wj@xkeshi.com>
2016-07-08 14:46:35 +0800

    linux下验证通过

--- install.js
+++ install.js
@@ -1,18 +1,26 @@
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-let exists = fs.existsSync || path.existsSync
+let exists = fs.existsSync || path.existsSync;
 
-let hook = path.resolve(__dirname, 'eslinthook')
-let root = path.resolve(__dirname, '..', '..')
-let git = path.resolve(root, '.git')
-    , hooks = path.resolve(git, 'hooks')
-    , precommit = path.resolve(hooks, 'pre-commit');
+let hook = path.resolve(__dirname, 'eslinthook');
+let root = path.resolve(__dirname, '../', '../');
+let git = path.resolve(root, '.git'), hooks = path.resolve(git, 'hooks'),
+    precommit = path.resolve(hooks, 'pre-commit');
 
+if (!exists(git) || !fs.lstatSync(git).isDirectory()) {
+  root = path.resolve(root, '../../../');
+  git = path.resolve(root, '.git');
+  hooks = path.resolve(git, 'hooks');
+  precommit = path.resolve(hooks, 'pre-commit');
+}
 
-if (!exists(git) || !fs.lstatSync(git).isDirectory()) return;
-if (!exists(hooks)) fs.mkdirSync(hooks);
+if (!exists(git) || !fs.lstatSync(git).isDirectory())
+  return;
+
+if (!exists(hooks))
+  fs.mkdirSync(hooks);
 
 fs.createReadStream(hook).pipe(fs.createWriteStream(precommit))
