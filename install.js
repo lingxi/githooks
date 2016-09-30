6a8ed74858b877d2fc2575ae3d0f2454f2ef6ee8
wj <wj@xkeshi.com>
2016-09-30 10:09:30 +0800

    新增commit-msg钩子  重新组织代码格式

--- install.js
+++ install.js
@@ -5,28 +5,35 @@ const fs = require('fs');
 const path = require('path');
 
 let exists = fs.existsSync || path.existsSync;
+let pkgName = require('./package.json').name
 
-let hook = path.resolve(__dirname, 'eslinthook');
+// 文件夹选择
+let hook = path.resolve(__dirname, 'hook');
 let root = exec('git rev-parse --show-toplevel').stdout.replace('\n', '');
 let git = path.resolve(root, '.git'),
-  hooks = path.resolve(git, 'hooks'),
-  precommit = path.resolve(hooks, 'pre-commit');
+  hooks = path.resolve(git, 'hooks');
 
+let tasks = ['pre-commit', 'commit-msg']
 
-if (!exists(git) || !fs.lstatSync(git).isDirectory())
-  return;
+if (!exists(git) || !fs.lstatSync(git).isDirectory()) {
+  console.log("请安装git")
+  process.exit(0)
+}
 
-if (!exists(hooks))
+if (!exists(hooks)) {
+  console.log("未找到.git/hooks文件夹，帮您创建")
   fs.mkdirSync(hooks);
+}
 
-// let f =fs.createWriteStream(precommit)
-// fs.createReadStream(hook).pipe(f)
-// console.log(hook);
-let hookFile = fs.readFileSync(hook).toString();
-hookFile = hookFile.replace('$NODEJS', process.execPath);
-hookFile = hookFile.replace('$NODEJSPATH',path.resolve(process.execPath,'../'))
-fs.writeFileSync(precommit, hookFile);
+tasks.forEach((value, index) => {
+  let task = path.resolve(hooks, value);
+  let hookFile = fs.readFileSync(hook).toString();
+  hookFile = hookFile.replace('$NODEJS', process.execPath);
+  hookFile = hookFile.replace('$NODEJSPATH', path.resolve(process.execPath, '../'))
+  hookFile = hookFile.replace('$TASK', `${pkgName}/scripts/${value}.js`)
+  fs.writeFileSync(task, hookFile);
 
+  fs.chmodSync(task, '777');
+  console.log(`${task} installed !`)
+})
 
-fs.chmodSync(precommit, '777');
-console.log('pre-commit installed')
