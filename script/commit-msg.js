91574896f1324ce660524c120d8c33fd00cbd5ec
kezhong <yekezhong@lingxi360.com>
2017-11-30 16:26:19 +0800

    MOD 提醒文案

--- script/commit-msg.js
+++ script/commit-msg.js
@@ -36,15 +36,15 @@ var error = function () {
 // 空一行
 <body>
 
-范例:
-  fix: feat(0429留言下单): add 'graphiteWidth' option
+范例：
+  feat(0429留言下单): 实现邮件定时发送后端接口
 
   巴拉巴拉说明具体情况
 
 -------------------------------------------------------------------
 
 说明：
-type（必需）、scope（可选）和subject（必需）。
+type（必需）、scope（可选）和 subject（必需）。
 <body>(可选)
 (1) type
       type用于说明 commit 的类别，只允许使用下面8个标识。
@@ -67,8 +67,8 @@ type（必需）、scope（可选）和subject（必需）。
 
 (4)Body 部分是对本次 commit 的详细描述，可以分成多行。下面是一个范例。
 
-More detailed explanatory text, if necessary.  Wrap it to 
-about 72 characters or so. 
+More detailed explanatory text, if necessary.  Wrap it to
+about 72 characters or so.
 
 Further paragraphs come after blank lines.
 
